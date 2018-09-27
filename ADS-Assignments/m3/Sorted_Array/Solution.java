29c4743bac513c67f1833a5eca9b9b815ba25358
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-09-26 21:43:55 -0600

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-Assignments/m3/Sorted_Array/Solution.java
+++ ADS-Assignments/m3/Sorted_Array/Solution.java
@@ -17,15 +17,15 @@ public final class Solution {
         int m = scan.nextInt();
         int n = scan.nextInt();
         scan.nextLine();
-        int[] first = new int[m];
-        int[] second = new int[n];
+        int[] firstarray = new int[m];
+        int[] secondarray = new int[n];
         int k = 0, i = 0, j = 0;
         String str = "";
         String[] s;
         if (m != 0) {
             s = scan.nextLine().split(",");
             for (String each : s) {
-                first[k++] = Integer.parseInt(each);
+                firstarray[k++] = Integer.parseInt(each);
             }
         } else {
             scan.nextLine();
@@ -34,26 +34,26 @@ public final class Solution {
         if (n != 0) {
             s = scan.nextLine().split(",");
             for (String each : s) {
-                second[k++] = Integer.parseInt(each);
+                secondarray[k++] = Integer.parseInt(each);
             }
         } else {
             scan.nextLine();
         }
         while (i < m && j < n) {
-            if (first[i] < second[j]) {
-                str += Integer.toString(first[i++]) + ",";
-            } else if (first[i] > second[j]) {
-                str += Integer.toString(second[j++]) + ",";
+            if (firstarray[i] < secondarray[j]) {
+                str += Integer.toString(firstarray[i++]) + ",";
+            } else if (firstarray[i] > secondarray[j]) {
+                str += Integer.toString(secondarray[j++]) + ",";
             } else {
-                str += Integer.toString(second[j++]) + ",";
+                str += Integer.toString(secondarray[j++]) + ",";
                 i++;
             }
         }
         while (i < m) {
-            str += Integer.toString(first[i++]) + ",";
+            str += Integer.toString(firstarray[i++]) + ",";
         }
         while (j < n) {
-            str += Integer.toString(second[j++]) + ",";
+            str += Integer.toString(secondarray[j++]) + ",";
         }
         System.out.println(str.substring(0, str.length() - 1));
     }
