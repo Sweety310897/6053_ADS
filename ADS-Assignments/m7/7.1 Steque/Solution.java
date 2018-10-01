aa650ec7f5f6576f1fe5eabf1ff46f7e01d9da2c
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-10-01 15:28:32 -0600

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS-Assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Node {
+	int data;
+	Node next;
+	Node() {
+
+	}
+	Node(int element) {
+		this.data = element;
+	}
+}
+
+class Stequeue {
+	private Node head;
+	private int size;
+	private Node tail;
+	Stequeue() {
+		head = new Node();
+		tail = new Node();
+		size = 0;
+	}
+	void insert(int item) {
+		if(size == 0) {
+			head.data = item;
+			head.next = null;
+			tail = head;
+			//System.out.println(start);
+			size++;
+			return;
+		}
+		Node temp = new Node();
+		temp.data = item;
+
+		temp.next = head;
+
+		head = temp;
+		//System.out.println(start);
+	}
+	void enqueueinsert(int item){
+		if(size == 0) {
+			tail.data = item;
+			tail.next = null;
+			head = tail;
+			size++;
+			return;
+		}
+		else {
+			Node oldtail = new Node();
+			oldtail.data = item;
+			oldtail.next = null;
+			tail.next = oldtail;
+			tail = oldtail;
+		}
+		size++;
+
+	}
+	public String display()
+
+    {
+        //System.out.print("\nSingly Linked List = ");
+        if (size != 0) {
+            String str = "";
+            Node temp1 = head;
+            while (temp1 != null) {
+                str += temp1.data + ", ";
+                // System.out.println(str);
+                temp1 = temp1.next;
+            }
+            //return str;
+            return str.substring(0, str.length() - 2);
+        }
+        return "[]";
+        // if (size == 0) 
+       
+    }
+	void delete() {
+		// if(!sta.isEmpty()) {
+		// 	System.out.println("empty");
+		
+		head = head.next;
+		size--;
+	}
+	boolean isEmpty() {
+		if(size == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	int size() {
+		return size;
+	}
+}
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = Integer.parseInt(scan.nextLine());
+		Stequeue sta = new Stequeue();
+		while(scan.hasNext()) {
+			String input = scan.nextLine();
+			String[] tokens = input.split(" ");
+			//System.out.println(Arrays.toString(tokens));
+			switch(tokens[0]) {
+				case "push" :sta.insert(Integer.parseInt(tokens[1]));
+				System.out.println( sta.display());
+				break;
+				case "pop" :sta.delete();
+				break;
+				// case "display": sta.display();
+				// break;
+				case "enqueue":
+				sta.enqueueinsert(Integer.parseInt(tokens[1]));
+				System.out.println(sta.display());
+				default:
+				break;
+			}
+		}
+		
+	}
+}
