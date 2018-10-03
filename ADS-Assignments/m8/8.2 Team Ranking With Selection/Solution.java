f9af1f4cd16a7a14266afa56d9b28b624f579fd5
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-10-02 19:32:58 -0600

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-Assignments/m8/8.2 Team Ranking With Selection/Solution.java
+++ ADS-Assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -1,14 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Sorting sort = new Sorting();
-		
 		while(sc.hasNext()) {
 			String[] input = sc.nextLine().split(",");
 			CricketRanking cricket = new CricketRanking(input[0],
-				Integer.parseInt(input[1]),Integer.parseInt(input[2]),Integer.parseInt(input[3]));
+				Integer.parseInt(input[1]), Integer.parseInt(input[2]),
+				Integer.parseInt(input[3]));
 			sort.add(cricket);
 		}
 		sort.SelectionSort();
