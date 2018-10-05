18a702f0edb56bb8512b593456f4ba653eef348d
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-10-04 18:49:57 -0600

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -2,43 +2,46 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//constructor.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		LinkedList ll = new LinkedList();
-		while (sc.hasNext()) {
-			String[] input = sc.nextLine().split(" ");
-			switch (input[0]) {
-				case "insertAt" :
-				try {
-					ll.insertAt(Integer.parseInt(input[1]),input[2]);
-					ll.display();	
-				}
-				catch (Exception e) {
-					System.out.println("Can't insert at this position.");
-				}
-				break;
-				case "reverse":
-				try {
-				ll.reverse();
-				ll.display();	
-				}
-				catch (Exception e) {
-					System.out.println("No elements to reverse.");
-				}
-				break;
-			}
-		}
-	}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList ll = new LinkedList();
+        while (sc.hasNext()) {
+            String[] input = sc.nextLine().split(" ");
+            switch (input[0]) {
+                case "insertAt" :
+                try {
+                    ll.insertAt(Integer.parseInt(input[1]),
+                        input[2]);
+                    ll.display();   
+                }
+                catch (Exception e) {
+                    System.out.println("Can't insert at this position.");
+                }
+                break;
+                case "reverse":
+                try {
+                ll.reverse();
+                ll.display();   
+                }
+                catch (Exception e) {
+                    System.out.println("No elements to reverse.");
+                }
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
