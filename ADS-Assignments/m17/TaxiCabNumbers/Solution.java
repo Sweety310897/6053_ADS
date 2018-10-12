50f8a490190a97d45b6c1a17eb744184b96ac721
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-10-12 16:10:53 -0600

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m17/TaxiCabNumbers/Solution.java
+++ ADS-Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 class CubeSum implements Comparable<CubeSum> {
-    private final int sum;
+    private final long sum;
     private final int i;
     private final int j;
 
     public CubeSum(int i, int j) {
-        this.sum = i*i*i + j*j*j;
+        this.sum = (long)i*i*i + (long) j*j*j;
         this.i = i;
         this.j = j;
     }
@@ -20,26 +21,55 @@ class CubeSum implements Comparable<CubeSum> {
     public int getj() {
         return this.j;
     }
+    public long getSum() {
+    	return this.sum;
+    }
     public String toString() {
-        return sum + " = " + i + "^3" + " + " + j + "^3";
+        //return sum + " = " + i + "^3" + " + " + j + "^3";
+        //return sum + " ";
+        return i + "^3" + j + "^3";
     }
 }
 public class Solution {
     public static void main(String[] args) {
-
-        int N = Integer.parseInt(args[0]);
-
+        Scanner scan = new Scanner(System.in);
+        int n = 1000;
+        
+        String[] input = scan.nextLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        // int temp = Integer.parseInt(input[0]);
         // initialize priority queue
         MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             pq.insert(new CubeSum(i, i));
+                 
+            //pq.insert(new CubeSum(i, i));
         }
-
+        //this is for first pair.
+        int pair = 1;
+        int nth = 0;
+        CubeSum previousc1 = new CubeSum(0,0);
         // find smallest sum, print it out, and update
         while (!pq.isEmpty()) {
-            CubeSum s = pq.delMin();
-            System.out.println(s);
-            if (s.getj() < N) pq.insert(new CubeSum(s.geti(), s.getj() + 1));
+            CubeSum currentc1 = pq.delMin();
+            if(currentc1.getSum() == previousc1.getSum()){
+            	pair++;
+            	if(pair == M) {
+            		nth++;
+            	}
+            	if(N == nth) {
+            		System.out.println(currentc1);
+            		break;
+            	}
+            }
+            else {
+            	pair = 1;
+            }
+            previousc1 = currentc1;
+            //System.out.println(s);
+            if (currentc1.getj() < N)
+            pq.insert(new CubeSum(currentc1.geti(), currentc1.getj() + 1));
         }
     }
 }
