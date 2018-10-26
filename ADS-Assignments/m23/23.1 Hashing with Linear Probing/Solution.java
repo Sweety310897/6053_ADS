117edf925e2cb1d591c18f2c2fe188fc7f5cb924
Sweety310897 <pulluri.shivani@msitprogram.net>
2018-10-26 17:04:05 -0600

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
+++ ADS-Assignments/m23/23.1 Hashing with Linear Probing/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 	/**
 	 * Constructs the object.
 	 */
@@ -17,23 +17,17 @@ public class Solution {
 	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int range = s.nextInt();
-		LinearProbingHashST<String, Integer> l = new LinearProbingHashST<>();
-		for(int i = 0; i <= range; i++) {
+		LinearProbingHashST<String, Integer> l = new
+		LinearProbingHashST<>();
+		for (int i = 0; i <= range; i++) {
 			String[] temp = s.nextLine().split(" ");
-			switch(temp[0]) {
+			switch (temp[0]) {
 				case "put": l.put(temp[1], Integer.parseInt(temp[2]));
 				break;
 				case "display":
-				// print keys
-				// for (String s1 : l.keys()) 
-				// System.out.println("{" + s1 + ":" + l.get(s1) + "}"); 
-    				//System.out.println(l.keys() + "keys");
     				if(l.size() == 0) {
-
     				System.out.println("{}");
-    		
     			} else {
-
     				String str = "";
     				for (String s1: l.keys()) {
     					str += s1 + ":" + l.get(s1) + ", ";
