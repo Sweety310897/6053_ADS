37baa72d1754477647e38ad28e7f4a091d134da3
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-02-12 23:26:20 -0700

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m24/2/Solution.java
+++ ADS-Assignments/m24/2/Solution.java
@@ -1,25 +1,79 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Solution {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int records = s.nextInt();
-		LinearProbingHashST<String, Integer> l = new
-        LinearProbingHashST<>();
-		s.nextLine();
-		for(int i = 0; i < records; i++) {
-			String studentdata = s.nextLine();
-		}
-		int search = s.nextInt();
-		for(int i =0; i < search; i++) {
-			String[] searchvalue = s.nextLine().split(" ");
-			switch(searchvalue[0]) {
-				// case "BE":
-				// break;
-			}
+		Scanner scan = new Scanner(System.in);
+		BST<Float,ArrayList<Student>> b = new BST<>();
+		int range = Integer.parseInt(scan.nextLine());
+		// scan.nextLine();
+		for(int i = 0; i < range; i++) {
+			String[] temp = scan.nextLine().split(",");
+			Student student = new Student(Integer.parseInt(temp[0]),temp[1],Float.parseFloat(temp[2]));
+			if(!b.contains(Float.parseFloat(temp[2]))) {
+                b.put(Float.parseFloat(temp[2]),new ArrayList<Student>());
+            }
+            b.get(Float.parseFloat(temp[2])).add(student);
+
 
 		}
+		// for(Float each:b.keys()) {
+			// System.out.println(b.get(each));
+			// System.out.println(each);
+		// }
+		int q = Integer.parseInt(scan.nextLine());
+
+		for(int i = 0; i < q; i++) {
+			
+			String[] tempq = scan.nextLine().split(" ");
+			if(tempq[0].equals("BE")) {
+				for(Float each: b.keys(Float.parseFloat(tempq[1]),Float.parseFloat(tempq[2]))) {
+					// System.out.println(b.get(each));
+					System.out.println(b.get(each));
+					// Student tempb = b.get(each);
+					// System.out.println(tempb);
+				}
+			}
+			if(tempq[0].equals("GE")) {
+				for(Float each: b.keys(Float.parseFloat(tempq[1]),b.max())) {
+					// System.out.println(b.get(each));
+					System.out.println(b.get(each));
+					// Student tempb = b.get(each);
+					// System.out.println(tempb);
+				}
+			}
+			if(tempq[0].equals("LE")) {
+				for(Float each: b.keys(b.min(),Float.parseFloat(tempq[2]))) {
+					// System.out.println(b.get(each));
+					System.out.println(b.get(each));
+					// Student tempb = b.get(each);
+					// System.out.println(tempb);
+				}
+			}
+		}
+		
 	}
 }
-class Hash {
+class Student {
+	int rollno;
+	String name;
+	float marks;
+	Student(int rollno, String name,float marks) {
+		this.rollno = rollno;
+		this.name = name;
+		this.marks = marks;
+	}
+	public int getRollno() {
+		return this.rollno;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public float getMarks() {
+		return this.marks;
+	}
+	public String toString() {
+		return this.rollno + "," + this.name +"," + this.marks;
+	}
 
 }
