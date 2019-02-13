a0a0be7d5c737d718593424b6086cfeef2d71399
Sweety310897 <pulluri.shivani@msitprogram.net>
2019-02-12 23:39:11 -0700

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-Assignments/m24/2/Solution.java
+++ ADS-Assignments/m24/2/Solution.java
@@ -26,40 +26,40 @@ public class Solution {
 		for(int i = 0; i < q; i++) {
 			
 			String[] tempq = scan.nextLine().split(" ");
-			// if(tempq[0].equals("BE")) {
-			// 	for(Float each: b.keys(Float.parseFloat(tempq[1]),Float.parseFloat(tempq[2]))) {
-			// 		// System.out.println(b.get(each));
-			// 		// System.out.println(b.get(each));
-			// 		for(Student e: b.get(each)) {
-			// 			System.out.println(e);
-			// 		}
-			// 		// Student tempb = b.get(each);
-			// 		// System.out.println(tempb);
-			// 	}
-			// }
-			// if(tempq[0].equals("GE")) {
-			// 	for(Float each: b.keys(Float.parseFloat(tempq[1]),b.max())) {
-			// 		// System.out.println(b.get(each));
-			// 		System.out.println(b.get(each));
-			// 		// for(Student e: b.get(each)) {
-			// 		// 	System.out.println(e);
-			// 		// }
-			// 		// Student tempb = b.get(each);
-			// 		// System.out.println(tempb);
-			// 	}
-			// }
-			if(tempq[0].equals("LE")) {
-				// for(Float each: b.keys(b.min(),Float.parseFloat(tempq[2]))) {
+			if(tempq[0].equals("BE")) {
+				for(Float each: b.keys(Float.parseFloat(tempq[1]),Float.parseFloat(tempq[2]))) {
 					// System.out.println(b.get(each));
-					System.out.println(b.min());
-					System.out.println(Float.parseFloat(tempq[2]));
-					// for(Student e: b.get(each)) {
-					// 	System.out.println(e);
-					// }
+					// System.out.println(b.get(each));
+					for(Student e: b.get(each)) {
+						System.out.println(e);
+					}
+					// Student tempb = b.get(each);
+					// System.out.println(tempb);
+				}
+			}
+			if(tempq[0].equals("GE")) {
+				for(Float each: b.keys(Float.parseFloat(tempq[1]),b.max())) {
+					// System.out.println(b.get(each));
+					// System.out.println(b.get(each));
+					for(Student e: b.get(each)) {
+						System.out.println(e);
+					}
+					// Student tempb = b.get(each);
+					// System.out.println(tempb);
+				}
+			}
+			if(tempq[0].equals("LE")) {
+				for(Float each: b.keys(b.min(),Float.parseFloat(tempq[1]))) {
+					// System.out.println(b.get(each));
+					// System.out.println(b.min());
+					// System.out.println(Float.parseFloat(tempq[1]));
+					for(Student e: b.get(each)) {
+						System.out.println(e);
+					}
 
 					// Student tempb = b.get(each);
 					// System.out.println(tempb);
-				// }
+				}
 			}
 		}
 		
